d43c0bb400718e5f6cb69482921433b9074966a4
Jaloliddin-Zokirov <jaloliddinzokirov1908@gmail.com>
2022-06-03 20:06:51 +0500

    Add js file

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var firstName = prompt ('Ismingiz kiriting');
+var question1 = prompt ('HTML to\'lliq nomi');
+var question2 = prompt ('CSS to\'lliq nomi');
+var question3 = prompt ('1vw nechchiga teng');
+var question4 = prompt ('SCSS to\'lliq nomi');
+var question5 = prompt ('1rem necha px ga teng');
+
+console.log('firstName ' + '\n' + 'question1 ' + '\n' + 'question2 ' + '\n' + 'question3 ' + '\n' + 'question4 ' + '\n' + 'question5 ');
